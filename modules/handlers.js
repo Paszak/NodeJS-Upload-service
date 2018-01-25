bc4bc8f1e0ea9783a1fc35b64dea2671d8090dee
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-01-25 18:07:44 +0100

    fix, add error condition

--- modules/handlers.js
+++ modules/handlers.js
@@ -4,7 +4,8 @@ var fs = require('fs'),
 exports.upload = function(request, response) {
 	console.log('Rozpoczynam obsługę żądania upload.');
 	var form = new formidable.IncomingForm();
-	form.parse(request, function(error, fields ,files) {
+	form.parse(request, function(err, fields ,files) {
+		if (err) throw err;
 		fs.renameSync(files.upload.path, 'test.png');
 		response.writeHead(200, {'Content-Type': 'text/html'});
 		response.write('received image:<br>');
@@ -16,6 +17,7 @@ exports.upload = function(request, response) {
 exports.welcome = function(request, response) {
 	console.log('Rozpoczynam obsługę żądania welcome.');
 	fs.readFile('templates/start.html', function(err, html) {
+		if (err) throw err;
 		response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 		response.write(html);
 		response.end();
@@ -30,6 +32,7 @@ exports.error = function(request, response) {
 
 exports.show = function (request, response) {
 	fs.readFile('test.png', 'binary', function(err, file) {
+		if (err) throw err;
 		response.writeHead(200, {"Content-Type": 'image/png'});
 		response.write(file , 'binary');
 		response.end();
